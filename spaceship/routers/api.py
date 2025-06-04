f3d37b868ac1083601c7ecb7a8e8a791a9af479c
Hell1on <107429202+bndrchuk-artem@users.noreply.github.com>
2025-06-04 18:50:34 +0300

    changed matrix return

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -8,17 +8,15 @@ router = APIRouter()
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
 
-@router.get('/matrix')
-def matrix_multiplication() -> dict:
-    # Генеруємо дві випадкові матриці 10x10
-    matrix_a = np.random.rand(10, 10)
-    matrix_b = np.random.rand(10, 10)
-    
-    # Перемножуємо матриці
-    product = np.dot(matrix_a, matrix_b)
-    
+@router.get("/matrix-multiply")
+def multiply_matrices():
+    matrix_a = np.random.randint(0, 10, (10, 10)).tolist()
+    matrix_b = np.random.randint(0, 10, (10, 10)).tolist()
+
+    product = (np.dot(np.array(matrix_a), np.array(matrix_b))).tolist()
+
     return {
-        "matrix_a": matrix_a.tolist(),
-        "matrix_b": matrix_b.tolist(),
-        "product": product.tolist()
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
     }
